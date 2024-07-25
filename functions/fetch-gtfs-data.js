960236ce6a1ec282511d190352a41769738d3743
clpdev <bgaiama1012141010@gmail.com>
2024-07-25 15:44:52 +0900

    functions5

--- functions/fetch-gtfs-data.js
+++ functions/fetch-gtfs-data.js
@@ -1,20 +1,20 @@
-import { transit_realtime } from 'gtfs-realtime-bindings';
+import gtfsRealtimeBindings from 'https://cdn.jsdelivr.net/npm/gtfs-realtime-bindings@1.1.1/+esm';
 
 export async function onRequest(context) {
-  const url = 'http://kumagaya.bus-go.com/GTFS-RT/encode_vehicle.php';
+    const url = 'http://kumagaya.bus-go.com/GTFS-RT/encode_vehicle.php';
 
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const arrayBuffer = await response.arrayBuffer();
+        const data = gtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(arrayBuffer));
+        return new Response(JSON.stringify(data), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        return new Response('Error: ' + error.message, { status: 500 });
     }
-    const arrayBuffer = await response.arrayBuffer();
-    const data = transit_realtime.FeedMessage.decode(new Uint8Array(arrayBuffer));
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    return new Response('Error: ' + error.message, { status: 500 });
-  }
 }
 
