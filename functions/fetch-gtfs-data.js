1d76fe913a971bd420a094f6a3383ef6ac254d5b
clpdev <bgaiama1012141010@gmail.com>
2024-10-11 16:57:55 +0900

    this requests only vehicle position fix0.0.3 add headers

--- functions/fetch-gtfs-data.js
+++ functions/fetch-gtfs-data.js
@@ -5,31 +5,37 @@ export async function onRequest(context) {
 
   console.log('Request URL:', url);
 
-  try {
-    const response = await fetch(url);
-    console.log('Response status:', response.status);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+try {
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json', // ここでリクエストヘッダーを設定
+      // 'Authorization': 'Bearer YOUR_TOKEN', // 他に必要なヘッダーがあればここに追加
     }
+  });
 
-    const buffer = await response.arrayBuffer();
-    console.log('Data received, length:', buffer.byteLength);
-    const feedMessage = transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
-    console.log('Decoded data:', feedMessage);
-
-    // Vehicle Positionsだけを抽出
-    const vehiclePositions = feedMessage.entity
-      .filter(entity => entity.vehicle)
-      .map(entity => entity.vehicle);
-
-    // JSON形式でVehicle Positionsだけを返す
-    const jsonData = JSON.stringify(vehiclePositions);
-
-    return new Response(jsonData, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    console.error('Error occurred:', error.message);
-    return new Response('Error: ' + error.message, { status: 500 });
+  console.log('Response status:', response.status);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
+
+  const buffer = await response.arrayBuffer();
+  console.log('Data received, length:', buffer.byteLength);
+  const feedMessage = transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
+  console.log('Decoded data:', feedMessage);
+
+  // Vehicle Positionsだけを抽出
+  const vehiclePositions = feedMessage.entity
+    .filter(entity => entity.vehicle)
+    .map(entity => entity.vehicle);
+
+  // JSON形式でVehicle Positionsだけを返す
+  const jsonData = JSON.stringify(vehiclePositions);
+
+  return new Response(jsonData, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+} catch (error) {
+  console.error('Error occurred:', error.message);
+  return new Response('Error: ' + error.message, { status: 500 });
+}
 }
