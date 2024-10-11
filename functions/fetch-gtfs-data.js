1aeea5c6fa78f0ad3ff82eafe41a1fad38528c9c
clpdev <bgaiama1012141010@gmail.com>
2024-10-11 15:28:38 +0900

    this requests only vehicle position fix0.0.2 add log

--- functions/fetch-gtfs-data.js
+++ functions/fetch-gtfs-data.js
@@ -3,14 +3,19 @@ import { transit_realtime } from 'gtfs-realtime-bindings';
 export async function onRequest(context) {
   const url = 'http://kumagaya.bus-go.com/GTFS-RT/encode_vehicle.php'; // Vehicle position endpoint
 
+  console.log('Request URL:', url);
+
   try {
     const response = await fetch(url);
+    console.log('Response status:', response.status);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
 
     const buffer = await response.arrayBuffer();
+    console.log('Data received, length:', buffer.byteLength);
     const feedMessage = transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
+    console.log('Decoded data:', feedMessage);
 
     // Vehicle Positionsだけを抽出
     const vehiclePositions = feedMessage.entity
@@ -24,6 +29,7 @@ export async function onRequest(context) {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
+    console.error('Error occurred:', error.message);
     return new Response('Error: ' + error.message, { status: 500 });
   }
 }
